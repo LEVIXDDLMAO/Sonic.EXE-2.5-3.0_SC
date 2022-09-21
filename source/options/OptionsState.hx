4c45e6d0497459139446bd334a27be7704d9872e
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-09-21 00:56:20 -0400

    no more note colors
    they aren't in the sonic.exe mod, I checked

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,15 +29,15 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = ['Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
 
 	function openSelectedSubstate(label:String) {
 		switch(label) {
-			case 'Note Colors':
-				openSubState(new options.NotesSubState());
+			// case 'Note Colors':
+			// 	openSubState(new options.NotesSubState());
 			case 'Controls':
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
